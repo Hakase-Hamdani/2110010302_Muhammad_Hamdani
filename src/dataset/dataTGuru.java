2ffab804fed7ef6bcbc7ebc756e10dc2743218b5
Hakase-Hamdani <mhdhamdani.formal@gmail.com>
2023-05-30 21:48:05 +0800

    add dataTGuru

---
+++ src/dataset/dataTGuru.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dataset;
+import java.util.ArrayList;
+/**
+ *
+ * @author hakase
+ */
+public class dataTGuru {
+    
+    private ArrayList<Integer> IdGuru;
+    private ArrayList<Integer> IdTWaliKelas;
+    private ArrayList<String> NamaGuru;
+    private ArrayList<String> MpGuru;
+    private ArrayList<String> AlamatGuru;
+    
+    public dataTGuru(){
+        IdGuru = new ArrayList<Integer>();
+        IdTWaliKelas = new ArrayList<Integer>();
+        NamaGuru = new ArrayList<String>();
+        MpGuru = new ArrayList<String>();
+        AlamatGuru = new ArrayList<String>();
+    }
+    
+}
