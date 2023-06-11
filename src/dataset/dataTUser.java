b7a93de349bacefc5e5c802b466b24712864e8c9
Hakase-Hamdani <mhdhamdani.formal@gmail.com>
2023-06-11 11:07:46 +0800

    compeleted the barebones basic idk what im doing send help

---
+++ src/dataset/dataTUser.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dataset;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hakase
+ */
+public class dataTUser {
+    private ArrayList<Integer> idAdmin;
+    private ArrayList<String> username;
+    private ArrayList<String> password;
+    
+    public dataTUser(){
+        idAdmin = new ArrayList<Integer>();
+        username = new ArrayList<String>();
+        password = new ArrayList<String>();
+    }
+    
+    //idAdmin
+    public void insertIdAdmin(Integer isi){
+        this.idAdmin.add(isi);
+    }
+    
+    public ArrayList<Integer> getRecordIdAdmin(){
+        return this.idAdmin;
+    }
+    
+    //username
+    public void insertUsername(String isi){
+        this.username.add(isi);
+    }
+    
+    public ArrayList<String> getRecordUsername(){
+        return this.username;
+    }
+    
+    //password
+    public void insertPassword(String isi){
+        this.password.add(isi);
+    }
+    
+    public ArrayList<String> getRecordPassword(){
+        return this.password;
+    }
+}
---
+++ src/dataset/dataTWaliKelas.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dataset;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hakase
+ */
+public class dataTWaliKelas {
+    private ArrayList<Integer> IdWaliKelas;
+    private ArrayList<String> NamaWaliKelas;
+    private ArrayList<Integer> IdTGuru;
+    
+    public dataTUser(){
+        idWaliKelas = new ArrayList<Integer>();
+        NamaWaliKelas = new ArrayList<String>();
+        IdTGuru = new ArrayList<Integer>();
+    }
+    
+    //idWaliKelas
+    public void insertIdWaliKelas(Integer isi){
+        this.IdWaliKelas.add(isi);
+    }
+    
+    public ArrayList<Integer> getRecordIdWaliKelas(){
+        return this.IdWaliKelas;
+    }
+    
+    //NamaWaliKelas
+    public void insertNamaWaliKelas(String isi){
+        this.NamaWaliKelas.add(isi);
+    }
+    
+    public ArrayList<String> getRecordNamaWaliKelas(){
+        return this.NamaWaliKelas;
+    }
+    
+    //IdTGuru
+    public void insertIdTGuru(Integer isi){
+        this.IdTGuru.add(isi);
+    }
+    
+    public ArrayList<Integer> getRecordIdTGuru(){
+        return this.IdTGuru;
+    }
+}
